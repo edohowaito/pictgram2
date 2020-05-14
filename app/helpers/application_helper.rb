cb4d8a9afeb181f82454d188f9baf77b612bef79
”suno” <stargazerastronomy2@gmail.com>
2020-05-14 22:56:44 +0000

    6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
 module ApplicationHelper
-
+    def logged_in?
+      !current_user.nil?
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   has_secure_password
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
   validates :password, presence: true, length: { minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
+  
+  has_many :topics
+  
 end
